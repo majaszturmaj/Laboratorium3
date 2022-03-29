f556a23eac0f6887af7ec2043cf5141c23f54cd1
Maja <dsw46144@student.dsw.edu.pl>
2022-03-29 19:10:51 +0200

    Tym razem dodano plik z zapisanymi komendami z ćwiczeń

---
+++ cwiczenia3-komendy.sh
@@ -0,0 +1,17 @@
+git commit
+git branch
+git checkout
+git merge
+git rebase
+bugFix
+git checkout
+HEAD
+# ^ oraz ^^ aby dostać się do starszych gałęzi (rodziców)
+#~(liczba) zamiast wpisywania kilku ^.
+git reset
+git revert
+git cherry-pick
+git rebase -i
+git commit --amend
+git tag
+git describe
